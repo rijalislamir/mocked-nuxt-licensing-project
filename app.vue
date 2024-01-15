8cf52c6dda633712799aae9bbb19ddbbbe7f2cf8
rijalislamir <rijalislamir@gmail.com>
2024-01-15 10:23:37 +0700

    add home views (hardcoded data)

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <ReceiptCard />
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center py-8">
+    <img src="~/assets/img/logo.svg" alt="Logo" />
+  </div>
+
+  <ReceiptCard />
+</template>
