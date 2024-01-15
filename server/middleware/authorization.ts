b9a7c0c7d8864da871eb5ee5e885575e0ff47cef
rijalislamir <rijalislamir@gmail.com>
2024-01-15 21:24:55 +0700

    fix authorization on update and delete

--- server/middleware/authorization.ts
+++ server/middleware/authorization.ts
@@ -1,11 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export default defineEventHandler((event) => {
-  const authRoute = "/api/receipts";
+  const authRouteRegex = /\/api\/receipts/;
   const authMethod = ["POST", "PUT", "DELETE"];
   const req = event.node.req;
 
-  if (req.originalUrl === authRoute && authMethod.includes(req.method || "")) {
+  if (
+    authRouteRegex.test(req.originalUrl || "") &&
+    authMethod.includes(req.method || "")
+  ) {
     try {
       const token = event.node.req.headers.authorization?.split(" ")[1] || "";
       const { jwtSecret } = useRuntimeConfig(event);
