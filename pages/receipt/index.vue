c020fa2b92ba21e8b78940bdd09868b171f8c6f1
rijalislamir <rijalislamir@gmail.com>
2024-01-16 15:11:57 +0700

    add pages to display the pdf

--- pages/receipt/index.vue
+++ pages/receipt/index.vue
@@ -1,11 +1,7 @@
 <template>
-  <div v-if="receipt" class="w-full h-full">
-    <iframe
-      src="/receipt.pdf"
-      class="w-full h-full"
-      loading="lazy"
-      title="PDF-file"
-    ></iframe>
+  <div v-if="receipt" class="h-full">
+    <embed v-if="isMobile" src="/receipt.pdf#navpanes=0" width="100%" />
+    <embed v-else src="/receipt.pdf#navpanes=0" width="100%" height="100%" />
   </div>
 
   <div v-else class="w-full h-full flex justify-center items-center">
@@ -14,6 +10,7 @@
 </template>
 
 <script setup lang="ts">
+const { isMobile } = useDevice();
 const route = useRoute();
 const { data: receipt } = await useFetch(`/api/receipts/hash/${route.query.v}`);
 </script>
